bb37b3f0140101ffc819bf3cc961c8ab480b4deb
NT Blok <ntblok.phd@gmail.com>
2025-09-07 16:59:48 -0700

    frontend base image

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to LangChain Chat</h1>
+        <p>Your application is ready to be developed!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
